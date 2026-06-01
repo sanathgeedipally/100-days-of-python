52ab6af7794cf2d49fd23512be8e3325a1b10ca3
sanathgeedipally <sanathgeedipally@gmail.com>
2026-06-01 14:05:16 +0530

    lists, adding and removing from list

---
+++ Day4/lists.py
@@ -0,0 +1,12 @@
+# adding to lists
+tech = ["google"]
+tech.append("Tcs")
+tech.insert(0,"Apple")
+tech.extend(["adobe","Tata"])
+print(tech)
+
+# removing from lists
+tech.pop(1)
+tech.remove("Tcs")
+tech.clear()
+print(tech)
